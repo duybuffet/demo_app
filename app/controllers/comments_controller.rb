20ec406d0d657fabed088a424e828bad1449b541
duyth1993 <duyth1993@gmail.com>
2015-10-08 15:28:49 +0700

    Fix some bugs

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
 	before_action :logged_in_user, only: :create
+	before_action :follow_or_owner, only: :create
 	def create
 		entry = Entry.find(params[:entry_id])
 		if !entry.nil?
@@ -16,4 +17,13 @@ class CommentsController < ApplicationController
 			redirect_to root_url
 		end
 	end
+
+	private 
+		def follow_or_owner
+			entry = Entry.find(params[:entry_id])
+			if entry.user != current_user && !current_user.following?(entry.user)
+				flash[:danger] = "You can not comment on this entry."
+        		redirect_to root_url
+			end
+		end
 end
